6a62bdeaa56805272d67b456f1cb24ec56f1c875
MiniRaccoonDog <atorres@spoonflower.com>
2018-06-25 13:53:20 -0400

    set up some basic configs, added firebase.js and modified index/app for router

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+// import 'firebase/auth'
+// import 'firebase/database'
+// import 'firebase/functions'
+
+var config = {
+  apiKey: 'AIzaSyB0IQttl1DeIKXmPZzqkQ3HLIZvZRu90FQ',
+  authDomain: 'momentum-final-cohort1.firebaseapp.com',
+  databaseURL: 'https://momentum-final-cohort1.firebaseio.com',
+  projectId: 'momentum-final-cohort1',
+  storageBucket: 'momentum-final-cohort1.appspot.com',
+  messagingSenderId: '711830112074'
+}
+
+firebase.initializeApp(config)
+
+export default firebase
