95946bcd5bcfd23fe02b126cce76bf4b1ba000ea
MiniRaccoonDog <atorres@spoonflower.com>
2018-06-26 14:06:52 -0400

    its almost working, previous/next saved-state/values

--- src/BasicQ.js
+++ src/BasicQ.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
-// import {Button} from 'primereact/components/button/Button'
+import {Button} from 'primereact/components/button/Button'
+
+import dogHouse from './Media/dogHouse.png'
 // import { BehaviorSubject } from 'rx'
 // import {SelectButton} from 'primereact/components/selectbutton/SelectButton'
 
@@ -7,45 +9,77 @@ class BasicQ extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      answer: '',
+      answer: this.props.savedanswers[this.props.index] ? this.props.savedanswers[this.props.index].answer : '',
       score: '',
-      value: ''
+      value: '',
+      index: ''
     }
     this.questionSubmit = this.questionSubmit.bind(this)
     this.handleOptionChange = this.handleOptionChange.bind(this)
+    this.goBack = this.goBack.bind(this)
   }
   handleOptionChange (event) {
+    console.log(event.target.dataset.index)
     this.setState({
       answer: event.target.name,
       score: event.target.dataset.score,
-      value: event.target.value
+      value: event.target.value,
+      index: event.target.dataset.index
     })
   }
 
-  questionSubmit (event) {
-    event.preventDefault()
-
-    this.props.addAnswer([{
+  questionSubmit () {
+    this.props.addAnswer(this.state.index, {
       answer: this.state.answer,
       score: this.state.score,
       value: this.state.value
-    }])
+    })
+    this.setState({
+      answer: this.props.savedanswers[this.props.index + 1] ? this.props.savedanswers[this.props.index + 1].answer : '',
+      score: this.props.savedanswers[this.props.index + 1] ? this.props.savedanswers[this.props.index + 1].score : '',
+      value: this.props.savedanswers[this.props.index + 1] ? this.props.savedanswers[this.props.index + 1].value : '',
+      index: ''
+    })
   }
+  goBack () {
+    this.props.prevAnswer()
+    const index = Math.max(this.props.index - 1, 0)
+    // const index = Math.max(this.props.index, 0)
+    // console.log('THIS IS GO BACK, this.props.index', this.props.index, 'index', index)
+    this.setState({
+      answer: this.props.savedanswers[index] ? this.props.savedanswers[index].answer : ''})
+  }
+
   render () {
+    console.log('this.state.answer', this.state.answer, 'this.props.savedanswers- ', this.props.savedanswers, 'props index-', this.props.index, 'old answer-', this.props.savedanswers[this.props.index])
     if (this.props.question.answer) {
       return (
-        <div>
-          <h1>{this.props.question.text}</h1>
-          <form>
-            {this.props.question.answer.map((entry, index) => {
-              return (
-                <div key={index}>
-                  <input type='radio' name={entry.text} data-score={entry.score} value={entry.value} onChange={(e) => this.handleOptionChange(e)} checked={this.state.answer === entry.text} />
-                  <label>{entry.text}</label>
-                </div>)
-            })}
-          </form>
-          <button>Previous</button><button type='submit' onClick={(e) => this.questionSubmit(e)}>Next</button>
+        <div className='megaWrapper'>
+          <div className='titleDiv'>
+            <header>
+              <img className='headerImage' src={dogHouse} alt='logo' />
+              <h2 className='header'>&nbsp;PupQuest Test</h2>
+            </header>
+          </div>
+          <div className='introPageDiv'>
+            <h1>{this.props.question.text}</h1>
+            <br />
+            <form>
+              {this.props.question.answer.map((entry, index) => {
+                return (
+                  <div key={index}>
+                    <input type='radio' name={entry.text} data-score={entry.score} value={entry.value} data-index={this.props.index} onChange={(e) => this.handleOptionChange(e)} checked={this.state.answer === entry.text} />
+                    <label>{entry.text}</label>
+                  </div>)
+              })}
+            </form>
+          </div>
+          <div className='navButtonDiv'>
+            {/* <Button className='navButton' onClick={this.props.removeAnswer} label='Previous Question' /> */}
+            {/* <Button className='navButton' onClick={this.props.prevAnswer} label='Previous Question' /> */}
+            <Button className='navButton' onClick={this.goBack} label='Previous Question' />
+            <Button className='navButton' onClick={this.questionSubmit} label='Next Question' />
+          </div>
         </div>
       )
     } else { return null }
--- src/Quiz.js
+++ src/Quiz.js
@@ -10,28 +10,62 @@ class Quiz extends Component {
       currentQ: {}
     }
     this.addAnswer = this.addAnswer.bind(this)
+    this.removeAnswer = this.removeAnswer.bind(this)
+    this.prevAnswer = this.prevAnswer.bind(this)
   }
 
-  addAnswer (newAnswer) {
-    const update = this.state.answers.concat(newAnswer)
+  addAnswer (answerIndex, newAnswer) {
+    console.log('add answer', answerIndex, newAnswer)
+    const update = this.state.answers
+    if (answerIndex === this.state.answers.length) {
+      update.splice(answerIndex, 0, newAnswer)
+    } else if (answerIndex !== this.state.answers.length) {
+      update.splice(answerIndex, 1, newAnswer)
+    }
+    // console.log({update})
     const totalLength = this.props.questions.length - 1
     const index = Math.min(update.length, totalLength)
     this.setState({
       answers: update,
-      currentQ: this.props.questions[index]
+      currentQ: this.props.questions[index],
+      index: index
+    })
+  }
+
+  removeAnswer () {
+    // console.log(this.state.answers)
+    this.state.answers.pop()
+    // console.log(this.state.answers)
+    const totalLength = this.props.questions.length - 1
+    const index = Math.min(this.state.answers.length, totalLength)
+    this.setState({
+      answers: this.state.answers,
+      currentQ: this.props.questions[index],
+      index: index
+    })
+  }
+  prevAnswer () {
+    const prev = this.props.questions.indexOf(this.state.currentQ) - 1
+    const index = Math.max(prev, 0)
+    // console.log('prev answer', prev, index)
+    this.setState({
+      currentQ: this.props.questions[index],
+      index: index
     })
   }
 
   componentDidMount () {
     const index = (this.state.answers.length)
-    this.setState({currentQ: this.props.questions[index]})
+    this.setState({
+      currentQ: this.props.questions[index],
+      index: index})
   }
 
   render () {
-    console.log(this.state.answers.length, this.props.questions.length)
+    // console.log('Quiz Render answer array', this.state.answers, 'answer array length', this.state.answers.length, 'props array length', this.props.questions.length)
     return (
       <div>
-        <BasicQ question={this.state.currentQ} addAnswer={this.addAnswer} />
+        <BasicQ question={this.state.currentQ} savedanswers={this.state.answers} index={this.state.index} addAnswer={this.addAnswer} removeAnswer={this.removeAnswer} prevAnswer={this.prevAnswer} />
       </div>
     )
   }
