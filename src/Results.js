83228022c4fe4941da479cbac24e2f374bef847c
emachaffie <emachaffie@gmail.com>
2018-07-05 09:22:21 -0400

    Changed questions for results page demo

--- src/Results.js
+++ src/Results.js
@@ -141,24 +141,13 @@ class Results extends Component {
               Show Detailed Results <FontAwesomeIcon icon='chevron-circle-down' />
               </button>
               <Accordion className='accordion hidden'>
-                <AccordionTab className='detailedResultsAccordion' header='Question that ranked red'>
-        The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughters wedding.
-        His beloved son Michael has just come home from the war, but does not intend to become part of his fathers business.
-        Through Michaels life the nature of the family business becomes clear. The business of the family is just like the head
-        of the family, kind and benevolent to those who give respect,
-        but given to ruthless violence whenever anything stands against the good of the family.
+                <AccordionTab className='detailedResultsAccordion' header='Are you allowed to visit the puppies?'>Uh oh… Visiting is the only way to know for sure what kind of place a puppy is coming from. Good breeders insist potential owners visit their puppies and will welcome you to see where they are raised. If this breeder will not let you visit, what could they be hiding? (Don’t be fooled by claims of “We don’t want our puppies to get sick”, walk away.)
                 </AccordionTab>
-                <AccordionTab header='Question that ranked yellow'>
-        Francis Ford Coppolas legendary continuation and sequel to his landmark 1972 film, The_Godfather parallels the young
-        Vito Corleone's rise with his son Michael's spiritual fall, deepening The_Godfathers depiction of the dark side of
-        the American dream. In the early 1900s, the child Vito flees his Sicilian village for America after the local Mafia kills his family.
-        Vito struggles to make a living, legally or illegally, for his wife and growing brood in Little Italy, killing the local Black Hand
-        Fanucci after he demands his customary cut of the tyro's business. With Fanucci gone, Vito's communal stature grows.
+                <AccordionTab header='Does the breeder ship puppies via airplane?'>
+                Young puppies are in a period of critical development. A flight is a potentially scary and dangerous experience. Heatstroke and crate phobias are real risks. Steer clear of any breeder who offers to ship you a pup!
                 </AccordionTab>
-                <AccordionTab header='Question that ranked green'>
-        After a break of more than 15 years, director Francis Ford Coppola and writer Mario Puzo returned to the well for this
-        third and final story of the fictional Corleone crime family. Two decades have passed, and crime kingpin Michael Corleone,
-        now divorced from his wife Kay has nearly succeeded in keeping his promise that his family would one day be completely legitimate.
+                <AccordionTab header='Is the breeder United States Department of Agriculture (USDA) licensed?'>
+                Excellent! The USDA oversees farms. If a breeder is USDA licensed, they are a puppy farm! Not having this license is a GOOD thing.
                 </AccordionTab>
               </Accordion>
             </div>
